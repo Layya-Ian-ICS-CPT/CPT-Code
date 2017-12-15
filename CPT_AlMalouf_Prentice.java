2ea6ddcc2eaaf9a76e52f73b8bff3f60ad4ff4a9
layyamalouf <34552398+layyamalouf@users.noreply.github.com>
2017-12-15 13:39:01 -0500

    n

--- CPT_AlMalouf_Prentice.java
+++ CPT_AlMalouf_Prentice.java
@@ -8,78 +8,77 @@ public class CPT_AlMalouf_Prentice
     {
 	c = new Console (190, 150);
 	char startInput = '.';
-	makeBallMove ();
-	makeTitleScreen (startInput);
-	if (startInput == ' ')
-	{
-	    levelOne ();
-	}
-
-
+	makeTitleScreen ();
     }
 
 
-    public static void makeTitleScreen (char input)
+    public static char makeTitleScreen ()
     {
 	int[] pointx = {250, 300, 200, 130};
 	int[] pointy = {200, 230, 400, 330};
-	int x = 260, y = 200, dx = 1, dy = -1;
-
-	//this section contains the graphics that creates the cannon
-	c.setColor (new Color (171, 149, 132));
-	c.fillRect (0, 0, c.maxx (), c.maxy ());
-	c.setColor (Color.black);
-	c.fillPolygon (pointx, pointy, 4);
-	c.fillOval (115, 315, 100, 100);
-	c.setColor (Color.yellow);
-	c.fillRect (130, 420, 100, 30);
-	c.fillRect (170, 360, 15, 80);
-
-	//this section contains all the draw string methods that print the game title and others
-	Font newFont = new Font ("optima regular", Font.BOLD + Font.ITALIC, 100);
-	c.setFont (newFont);
-	c.setColor (Color.red);
-	c.drawString ("Cannon Launch", 240, 400);
-	Font newFont2 = new Font ("optima regular", Font.BOLD + Font.ITALIC, 30);
-	c.setFont (newFont2);
-	c.setColor (Color.black);
-	c.drawString ("Layya Al Malouf & Ian Prentice", 50, 800);
-	//c.drawString ("Press enter to begin", 450, 450);
-	//c.print (" ");
-	//input = c.readChar ();
+	int x = 300, y = 150, dx = 1, dy = -1;
+	char input;
 
 	while (true)
 	{
+	    //this section contains the graphics that creates the cannon
+	    c.setColor (new Color (171, 149, 132));
+	    c.fillRect (0, 0, c.maxx (), c.maxy ());
+	    c.fillRect (0, 700, c.maxx (), c.maxy ());
+	    c.setColor (Color.black);
+	    c.fillPolygon (pointx, pointy, 4);
+	    c.fillOval (115, 315, 100, 100);
+	    c.setColor (Color.yellow);
+	    c.fillRect (130, 420, 100, 30);
+	    c.fillRect (170, 360, 15, 80);
+
+	    //this section contains all the draw string methods that print the game title and others
+	    Font newFont = new Font ("Arial", Font.BOLD + Font.ITALIC, 100);
+	    c.setFont (newFont);
+	    c.setColor (Color.red);
+	    c.drawString ("Cannon Launch", 240, 400);
+	    Font newFont2 = new Font ("optima regular", Font.BOLD + Font.ITALIC, 30);
+	    c.setFont (newFont2);
+	    c.setColor (Color.black);
+	    c.drawString ("Layya Al Malouf & Ian Prentice", 50, 800);
+	    c.drawString ("Press enter to begin", 450, 450);
+	    c.print (" ");
+	    input = c.readChar ();
+
 	    for (int i = 0 ; i < 100000 ; i++)
 
 		{
 		    c.setColor (Color.white);
-		    c.drawOval (x, y, 50, 50);
-		    x += dx;
-		    y += dy;
-		    c.setColor (new Color (171, 149, 132));
-		    c.drawOval (x, y, 50, 50);
-
-		    if (y == 500)
+		    c.fillOval (x, y, 50, 50);
+		    if (y < 25)
 		    {
-			y += -dy;
+			c.setColor (new Color (171, 149, 132));
+			c.fillOval (x, y, 50, 50);
+			break;
 		    }
+		    delay (30);
+		    c.setColor (new Color (171, 149, 132));
+		    c.fillOval (x, y, 50, 50);
+		    x = x + 10;
+		    y = y - 5;
+
 		}
+	    return input;
 	}
-	//return input;
-
     }
 
 
-    public static void makeBallMove ()
+    public static void delay (int time)
     {
-	int xpoint = 150, ypoint = 100;
+	try
+	{
+	    Thread.sleep (time);
+	}
 
-    }
-
-
-    public static void levelOne ()
-    {
-	d.println ("level one");
+	catch (InterruptedException e)
+	{
+	}
     }
 }
+
+
