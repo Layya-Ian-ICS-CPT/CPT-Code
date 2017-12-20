f8bbe700f7d640fa1a6c2780a5ddc33bcafaf204
layyamalouf <34552398+layyamalouf@users.noreply.github.com>
2017-12-20 13:33:47 -0500

    i

--- CPT_AlMalouf_Prentice.java
+++ CPT_AlMalouf_Prentice.java
@@ -1,6 +1,10 @@
 import java.awt.event.*;
 import java.applet.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.net.URL;
 
 public class CPT_AlMalouf_Prentice extends Applet implements KeyListener, MouseListener
 {
@@ -13,7 +17,7 @@ public class CPT_AlMalouf_Prentice extends Applet implements KeyListener, MouseL
 	menuStart = new Button ("Press to begin");
 	menuStart.setBounds (470, 450, 250, 50);
 	nextLevel = new Button ("Next Level");
-	nextLevel.setBounds (500, 500, 100,40);
+	nextLevel.setBounds (500, 500, 100, 40);
 	restart = new Button ("Restart");
 	restart.setBounds (590, 500, 100, 40);
 	add (menuStart);
@@ -95,14 +99,58 @@ public class CPT_AlMalouf_Prentice extends Applet implements KeyListener, MouseL
 	    y = y - 5;
 	}
     }
-    
+
+
     public void levelOne (Graphics g)
     {
-    g.drawRect (100,100,100,100);
+
+	g.drawRect (100, 325, 100, 100);
+	drawCannon (g);
+
     }
 
 
-    public static void delay (int time)
+    public void getImage (BufferedImage[] img)
+    {
+	String[] links = {"http://i.imgur.com/OaSlqSR.png",
+	    "http://i.imgur.com/STgsftB.png",
+	    "http://i.imgur.com/n7ZOq7y.png",
+	    "http://i.imgur.com/eKW7Jfr.png",
+	    "http://i.imgur.com/ZjtQosM.png",
+	    "http://i.imgur.com/FSnI1Pn.png",
+	    "http://i.imgur.com/Vu7b24F.png",
+	    "http://i.imgur.com/Metb2lR.png"};
+
+	for (int j = 0 ; j < img.length ; j++)
+	{
+	    try
+	    {
+		URL url = new URL (links [j]);
+		img [j] = ImageIO.read (url);
+	    }
+	    catch (IOException e)
+	    {
+
+	    }
+
+	}
+
+    }
+
+
+    public void drawCannon (BufferedImage[] image, Graphics g)
+    {
+	for (int m = 0 ; m < image.length ; m++)
+	{
+	    g.drawImage (image [m], 100, 100, null);
+	    delay (100);
+	    g.setColor (Color.white);
+	    g.fillRect (100, 100, 300, 300);
+	}
+    }
+
+
+    public void delay (int time)
     {
 	try
 	{
@@ -171,6 +219,6 @@ public class CPT_AlMalouf_Prentice extends Applet implements KeyListener, MouseL
 	{
 	    menuSelect = menuSelect;
 	}
-	repaint();
+	repaint ();
     }
 }
